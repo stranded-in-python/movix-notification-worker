4c1b9a868efc2409f3f825753940644a4ad8cc0d
Viktor-Gostyaikin <gostyaikin@yandex.ru>
2023-08-30 18:00:34 +0300

    fix: docs

--- src/services/publisher.py
+++ src/services/publisher.py
@@ -12,9 +12,6 @@ class RabbitMQPublisher:
     def __init__(self, properties: PublisherProperties = publisher_properties):
         """Setup the publisher object, passing in the URL we will use
         to connect to RabbitMQ.
-
-        :param str amqp_url: The URL for connecting to RabbitMQ
-
         """
         self._connection = None
         self._channel = None
@@ -34,22 +31,15 @@ class RabbitMQPublisher:
         self.ROUTING_KEY = properties.routing_key
 
     async def connect(self):
-        """This method connects to RabbitMQ, returning the connection handle.
-        When the connection is established, the on_connection_open method
-        will be invoked by pika.
-
-        :rtype: pika.SelectConnection
-
+        """
+        This method connects to RabbitMQ, returning the connection handle.
         """
         LOGGER.info("Connecting to %s", self._url)
         self._connection = await aio_pika.connect_robust(self._url)
 
     async def open_channel(self):
         """This method will open a new channel with RabbitMQ by issuing the
-        Channel.Open RPC command. When RabbitMQ confirms the channel is open
-        by sending the Channel.OpenOK RPC reply, the on_channel_open method
-        will be invoked.
-
+        Channel.Open RPC command.
         """
         LOGGER.info("Creating a new channel")
         self._channel = await self._connection.channel()
