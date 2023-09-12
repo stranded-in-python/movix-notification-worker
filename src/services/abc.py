4c4d2b3fb69c1d602ff3476d273e5e406c798cb7
serg <sergeusprecious@gmail.com>
2023-09-12 18:13:28 +0300

    initial

--- src/services/abc.py
+++ src/services/abc.py
@@ -21,3 +21,12 @@ class NotificationServiceABC(ABC):
 
     def send_message():
         ...
+
+class NotificationSettingsServiceABC(ABC):
+    @abstractmethod
+    async def change_channel_sending(self, channel: str, enabled: bool, user_id: UUID):
+        raise NotImplementedError
+    
+    @abstractmethod
+    async def change_notification_sending(self, notification_id: UUID, disabled: bool, user_id: UUID):
+        raise NotImplementedError
